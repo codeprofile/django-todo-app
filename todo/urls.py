fe042a9dd9816891ec6453d5c970de3f9c15b30f
codeprofile <lax.sarki@gmail.com>
2024-01-23 21:52:59 +0530

    todo final commit

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,16 @@
-from  django.urls import path
+from django.urls import path
 from . import views
 
-urlpatterns =[
-    path("addTask/",views.addTask,name="addTask"),
+urlpatterns = [
+    # Add Task
+    path("addTask/", views.addTask, name="addTask"),
+    # Mark as done
+    path("mark_as_done/<int:pk>/", views.mark_as_done, name="mark_as_done"),
+    # Mark as undone
+    path("mark_as_undone/<int:pk>/", views.mark_as_undone, name="mark_as_undone"),
+    # Edit Feature
+    path("edit_task/<int:pk>/",views.editTask,name="editTask"),
+    # Delete task
+    path("delete_task/<int:pk>/",views.deleteTask,name="deleteTask")
 
 ]
